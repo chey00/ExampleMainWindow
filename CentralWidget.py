912c5a154da3954099fb7296a748927e212a6703
Christian Hey <c.hey@sbs-herzogenaurach.de>
2024-04-15 13:21:14 +0200

    Added minor changeses

--- CentralWidget.py
+++ CentralWidget.py
@@ -1,3 +1,5 @@
+from PyQt6.QtCore import QSize, pyqtSlot
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QWidget, QPushButton, QGridLayout, QLineEdit
 
 
@@ -7,10 +9,18 @@ class CentralWidget(QWidget):
 
         push_button_abc = QPushButton()
         push_button_abc.setText("Schreibe abc")
+
+        push_button_abc.setIcon(QIcon("ausgebucht.png"))
+        push_button_abc.setIconSize(QSize(128, 128))
+
         push_button_abc.released.connect(self.add_abc)
         push_button_abc.pressed.connect(self.temp)
 
         push_button_xyz = QPushButton("Schreibe xyz")
+
+        push_button_xyz.setIcon(QIcon("schild.png"))
+        push_button_xyz.setIconSize(QSize(128, 128))
+
         push_button_xyz.released.connect(self.add_xyz)
         push_button_xyz.pressed.connect(self.temp)
 
@@ -23,11 +33,14 @@ class CentralWidget(QWidget):
 
         self.setLayout(layout)
 
+    @pyqtSlot
     def add_abc(self):
         self.line_edit.setText("abc")
 
+    @pyqtSlot
     def add_xyz(self):
         self.line_edit.setText("xyz")
 
+    @pyqtSlot
     def temp(self):
         self.line_edit.setText("Button ist gedrückt...")
